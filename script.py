6b12326619afa112644a2925bc5456971d58cbd2
Tonho <antonioreverso.com@gmail.com>
2025-02-26 20:12:12 -0300

    change to rich

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 import psutil
-from pprint import pprint as pp
+from rich import print
 
 def kill_process():
     for proc in psutil.process_iter(['pid', 'name']):
         if(proc.info['name'] == "Telegram"):
             p = psutil.Process(proc.info['pid'])
-            pp(f"{proc.info['name']} terminated at PID: {proc.info['pid']}")
+            print(f"{proc.info['name']} terminated at PID: {proc.info['pid']}[bold magenta]")
             p.terminate()
-            p.wait()
+            p.wait() 
+
+
 
