acc4bc946e80212d8ce13badc8d0cce68d915be2
Tonho <antonioreverso.com@gmail.com>
2025-02-26 21:33:10 -0300

    work_mode function

--- script.py
+++ script.py
@@ -1,5 +1,11 @@
 import psutil
+import sys,time, os
 from rich import print
+from rich.console import Console
+from alive_progress import alive_bar
+import time
+
+console = Console()
 
 def kill_process():
     for proc in psutil.process_iter(['pid', 'name']):
@@ -8,6 +14,18 @@ def kill_process():
             print(f"{proc.info['name']} terminated at PID: {proc.info['pid']}[bold magenta]")
             p.terminate()
             p.wait() 
+
+def work_mode(hours: float):
+        total_timer = int(hours * 3600)
+        print(f"Iniciando timer de [bold green]{time} hora(s)")
+        with alive_bar(total_timer) as bar:
+            for i in range(total_timer):
+                time.sleep(1)
+                kill_process()
+                bar()
+
+work_mode(0.01)
+
 
 
 
